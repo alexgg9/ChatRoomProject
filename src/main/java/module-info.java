7586cbe39e0de47e9a13a76bf81aaa992155c503
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-10-05 17:19:40 +0200

    OperativoElNuevoRepositorio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
 module iesfranciscodelosrios.chatroomsproject {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-                        
+    requires java.xml.bind;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+
     opens iesfranciscodelosrios.chatroomsproject to javafx.fxml;
     exports iesfranciscodelosrios.chatroomsproject;
+    exports iesfranciscodelosrios.chatroomsproject.controllers;
+    opens iesfranciscodelosrios.chatroomsproject.model.domain to java.xml.bind;
+    opens iesfranciscodelosrios.chatroomsproject.controllers to javafx.fxml;
 }
