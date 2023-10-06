8bebe69e340f00b448dafda65b1751a5e1a98b69
fcar16 <fcarlop2710@g.educaand.es>
2023-10-06 14:32:29 +0200

    Login falta enlazar

--- src/main/java/iesfranciscodelosrios/chatroomsproject/App.java
+++ src/main/java/iesfranciscodelosrios/chatroomsproject/App.java
@@ -12,7 +12,6 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("VistaLoginChat3mos.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 440);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
