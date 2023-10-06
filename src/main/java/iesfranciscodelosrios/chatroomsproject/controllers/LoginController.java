01ecff6f1a2fe78829b0088d88e6f4766cb6ef20
fcar16 <fcarlop2710@g.educaand.es>
2023-10-06 20:40:42 +0200

    ChatController

--- src/main/java/iesfranciscodelosrios/chatroomsproject/controllers/LoginController.java
+++ src/main/java/iesfranciscodelosrios/chatroomsproject/controllers/LoginController.java
@@ -6,7 +6,9 @@ import iesfranciscodelosrios.chatroomsproject.model.DAO.UserDAO;
 import iesfranciscodelosrios.chatroomsproject.model.domain.User;
 import iesfranciscodelosrios.chatroomsproject.shareFolder.ConfigManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import java.io.IOException;
@@ -16,8 +18,11 @@ import java.util.ResourceBundle;
 public class LoginController implements Initializable {
 
 
+    public LoginController() {
+        username=getUsername();
+    }
 
-
+    public  String username;
     @FXML
     private TextField usernameField;
 
@@ -29,6 +34,9 @@ public class LoginController implements Initializable {
 
     private ChatController chatController;
 
+    static String usuario;
+
+    static String Room;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -47,25 +55,28 @@ public class LoginController implements Initializable {
         UserDAO userDAO = new UserDAO();
         ChatRoomDAO chatRoomDAO = new ChatRoomDAO();
         String path = ConfigManager.readSharedFolderPath();
-        String username = usernameField.getText();
-        String sala = salaField.getText();
+         usuario = usernameField.getText();
+         Room = salaField.getText();
         User user = new User(username);
         userDAO.addUser(user);
         userDAO.saveUsers(path+"users.xml");
 
-        chatRoomDAO.createChatRoom(sala);
-        chatRoomDAO.saveChat(path + sala);
+        chatRoomDAO.createChatRoom(Room);
+        chatRoomDAO.saveChat(path + Room);
         chatRoomDAO.joinChat(username);
+
         switchToChat();
-
-
 
     }
 
+    public String getUsername(){
+        return this.username;
+    }
     @FXML
     private void switchToChat() throws IOException {
         ChatController chatController = new ChatController();
         //chatController.setUserInfo();
+
         App.setRoot("chat");
 
     }
