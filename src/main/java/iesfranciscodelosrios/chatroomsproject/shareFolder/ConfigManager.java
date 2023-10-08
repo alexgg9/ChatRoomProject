416a7acfca2aed0e026e587adc8714f7b7f8f9ac
Sergio Cubero <scubcar521@gmail.es>
2023-10-08 21:13:27 +0200

    Actualización 2

--- src/main/java/iesfranciscodelosrios/chatroomsproject/shareFolder/ConfigManager.java
+++ src/main/java/iesfranciscodelosrios/chatroomsproject/shareFolder/ConfigManager.java
@@ -11,19 +11,28 @@ public class ConfigManager {
 
     private String configFilePath;
 
+    // Constructor para inicializar la ruta del archivo de configuración
     public ConfigManager(String configFilePath) {
         this.configFilePath = configFilePath;
     }
+
+    // Método para leer la ruta de la carpeta compartida desde el archivo de configuración
     public static String readSharedFolderPath() {
         try {
             // Crear el contexto JAXB y el unmarshaller
             JAXBContext context = JAXBContext.newInstance(Config.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            // Se utiliza la ruta del archivo de configuración proporcionada en el constructor
             File configFile = new File("config/config.xml");
+
+            // Deserializar el archivo de configuración en un objeto Config
             Config config = (Config) unmarshaller.unmarshal(configFile);
+
+            // Devolver la ruta de la carpeta compartida desde el objeto Config
             return config.getSharedFolderPath();
         } catch (JAXBException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Imprimir información de la excepción en caso de error
             return null;
         }
     }
